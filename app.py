0309f96e8d68c82b3554c4adb99aacaee3557a32
JunyiCHEN <junyichen@d24h.hk>
2021-08-03 23:11:03 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import pandas as pd
+from sklearn.manifold import TSNE
+import json
+import plotly
+import plotly.express as px
+app = Flask(__name__)
+@app.route('/')
+def notdash():
+    df = px.data.iris()
+    features = df.loc[:, :'petal_width']
+    tsne = TSNE(n_components=2, random_state=0)
+    projections = tsne.fit_transform(features)
+
+    fig = px.scatter(
+        projections, x=0, y=1,
+        color=df.species, labels={'color': 'species'}
+    )
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('notdash.html', graphJSON=graphJSON)
