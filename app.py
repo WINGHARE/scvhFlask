765a5efc3275951cc25515077bdeb3176392d82f
JunyiCHEN <junyichen@d24h.hk>
2021-08-04 18:07:34 +0800

    Edit the debugger and add umap display

--- app.py
+++ app.py
@@ -4,17 +4,36 @@ from sklearn.manifold import TSNE
 import json
 import plotly
 import plotly.express as px
+import scanpy as sc
+from datetime import datetime
+import re
 app = Flask(__name__)
 @app.route('/')
 def notdash():
-    df = px.data.iris()
-    features = df.loc[:, :'petal_width']
-    tsne = TSNE(n_components=2, random_state=0)
-    projections = tsne.fit_transform(features)
+    adata=sc.read_h5ad("data/gccells.h5ad")
+    df = adata.obs
+    projections = adata.obsm['X_umap']
 
     fig = px.scatter(
         projections, x=0, y=1,
-        color=df.species, labels={'color': 'species'}
+        color=df.leiden, labels={'color': 'cluster'}
     )
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('scatter.html', graphJSON=graphJSON)
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
